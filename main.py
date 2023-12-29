a9810f22cef4720ff7e4c3af52b6edbe07157d2a
Joe O'Meara <joemetheus@protonmail.com>
2023-12-28 22:54:42 -0700

    add line and point classes

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 
 def main():
     win = Window(800, 600)
+    
+    # draw lines to test Line and Point classes
+    l = Line(Point(50, 50), Point(400, 400))
+    win.draw_line(l, "black")
+
     win.wait_for_close()
+
 
 main()
